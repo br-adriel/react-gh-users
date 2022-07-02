0dc1d2636c68ac592f6591380c83c77d155ab4e3
Adriel <adriel.fariasan@gmail.com>
2022-07-02 16:38:13 -0300

    Adiciona compoenente Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <div>App</div>;
-}
+import Layout from './components/Layout';
+
+const App = () => {
+  return (
+    <main>
+      <Layout>
+        <div>
+          <img
+            src='https://avatars.githubusercontent.com/u/49590887?v=4'
+            alt='User profile'
+          />
+          <h1>User name</h1>
+          <h3>username</h3>
+          <span>benits</span>
+          <div>
+            <h4>followers</h4>
+            <span>4</span>
+          </div>
+          <div>
+            <h4>starred</h4>
+            <span>4</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>4</span>
+          </div>
+        </div>
+        <div>repos</div>
+        <div>stars</div>
+      </Layout>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+const Layout = ({ children }) => {
+  return (
+    <section>
+      <header>HEADER</header>
+      {children}
+    </section>
+  );
+};
+
+export default Layout;
