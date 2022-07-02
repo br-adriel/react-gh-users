0a81e7cc28619f213ca78b7ab177575c1324d554
Adriel <adriel.fariasan@gmail.com>
2022-07-02 17:45:04 -0300

    Adiciona componente de estilizacao global

---
+++ src/global/GlobalCss.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+import 'normalize.css';
+
+export const GlobalCss = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'normalize.css';
+import { GlobalCss } from './global/GlobalCss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalCss />
     <App />
   </React.StrictMode>
 );
