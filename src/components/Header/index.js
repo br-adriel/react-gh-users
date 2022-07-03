1eb423f816f8803a4afcdc1b881485109d570703
Adriel <adriel.fariasan@gmail.com>
2022-07-03 16:13:42 -0300

    Implementa busca apartir de form

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled';
+import useGithub from '../../hooks/github-hooks';
 
 const Header = () => {
+  const [usernameForSearch, setUsernameForSearch] = useState('');
+
+  const { getUser } = useGithub();
+
+  const submitGetUser = (e) => {
+    e.preventDefault();
+    if (!usernameForSearch.length) return;
+    return getUser(usernameForSearch);
+  };
+
   return (
     <header>
-      <S.SearchForm>
+      <S.SearchForm onSubmit={submitGetUser}>
         <label htmlFor='searchInput'>Search</label>
-        <input type='search' name='search' id='searchInput' />
+        <input
+          type='search'
+          name='search'
+          id='searchInput'
+          value={usernameForSearch}
+          onChange={(e) => {
+            setUsernameForSearch(e.target.value);
+          }}
+          required
+          minLength='1'
+        />
         <button type='submit'>Search</button>
       </S.SearchForm>
     </header>
