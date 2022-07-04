27b149117f2160f14a2edad1b96dc64f5c6995ad
Adriel <adriel.fariasan@gmail.com>
2022-07-03 21:07:08 -0300

    Adiciona estilizacao global

--- src/global/GlobalCss.js
+++ src/global/GlobalCss.js
@@ -2,10 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 import 'normalize.css';
 
 export const GlobalCss = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: 'Barlow', sans-serif;
-    min-height: 100vh;
     background: ${({ theme }) => theme.bg};
     color: ${({ theme }) => theme.text};
+    
+    #root {
+      width: 100%;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 10px;
+    }
   }
 `;
