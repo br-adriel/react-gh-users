3c5a728ee79085d1f0543b5b229efb60223eb6f8
Adriel <adriel.fariasan@gmail.com>
2022-07-03 21:32:50 -0300

    Altera cor de destaque da pagina

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -2,12 +2,12 @@ export const lightTheme = {
   bg: '#fdfeff',
   bgAlt: '#fff',
   text: '#000',
-  accent: '#7C8CFC',
+  accent: '#34c436',
 };
 
 export const darkTheme = {
   bg: '#1f1f1f',
   bgAlt: '#111',
   text: '#fff',
-  accent: '#7C8CFC',
+  accent: '#34c436',
 };
