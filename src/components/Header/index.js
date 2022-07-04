bbe76d5606d0ecd56ad574d6afe92bf48a182af1
Adriel <adriel.fariasan@gmail.com>
2022-07-03 23:02:55 -0300

    Move formulario de busca para componente proprio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,11 @@
-import React, { useState } from 'react';
+import SearchBar from '../SearchBar';
 import * as S from './styled';
-import useGithub from '../../hooks/github-hooks';
 
 const Header = () => {
-  const [usernameForSearch, setUsernameForSearch] = useState('');
-
-  const { getUser } = useGithub();
-
-  const submitGetUser = (e) => {
-    e.preventDefault();
-    if (!usernameForSearch.length) return;
-    return getUser(usernameForSearch);
-  };
-
   return (
     <S.Header>
       <h1>GH Users</h1>
-      <S.SearchForm onSubmit={submitGetUser}>
-        <label htmlFor='searchInput'>Search</label>
-        <input
-          type='search'
-          name='search'
-          id='searchInput'
-          value={usernameForSearch}
-          onChange={(e) => {
-            setUsernameForSearch(e.target.value);
-          }}
-          required
-          minLength='1'
-        />
-        <button type='submit'>Search</button>
-      </S.SearchForm>
+      <SearchBar />
     </S.Header>
   );
 };
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,45 +1,5 @@
 import styled from 'styled-components';
 
-export const SearchForm = styled.form`
-  display: flex;
-  width: 100%;
-  justify-content: flex-end;
-  gap: 8px;
-
-  label {
-    display: none;
-  }
-
-  input,
-  button {
-    border-radius: 4px;
-    padding: 4px 6px;
-    font-size: 1rem;
-  }
-
-  input {
-    border: 1px solid #ccc;
-    flex-grow: 1;
-
-    &:focus {
-      outline: none;
-    }
-  }
-
-  @media screen and (min-width: 768px) {
-    input {
-      flex-grow: 0;
-      width: 40%;
-    }
-  }
-
-  button {
-    border: none;
-    padding: 6px 12px;
-    cursor: pointer;
-  }
-`;
-
 export const Header = styled.header`
   background: ${({ theme }) => theme.bgAlt};
   width: 100%;
