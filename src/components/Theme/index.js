1ccbda2ec2dea7ec0af91572df86ecfb825740fc
Adriel <adriel.fariasan@gmail.com>
2022-07-03 21:06:41 -0300

    Adiciona cor de fundo alternativa

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,11 +1,13 @@
 export const lightTheme = {
   bg: '#fdfeff',
+  bgAlt: '#fff',
   text: '#000',
   accent: '#7C8CFC',
 };
 
 export const darkTheme = {
   bg: '#1f1f1f',
+  bgAlt: '#111',
   text: '#fff',
   accent: '#7C8CFC',
 };
