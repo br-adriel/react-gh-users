942000264519174c42f6ee05f60caa631a556db4
Adriel <adriel.fariasan@gmail.com>
2022-07-05 23:26:40 -0300

    Adiciona componente UserDataItem

--- src/components/UserDataPanel/index.js
+++ src/components/UserDataPanel/index.js
@@ -1,37 +1,42 @@
-import React from 'react';
 import useGithub from '../../hooks/github-hooks';
+import UserDataItem from '../UserDataItem';
 import * as S from './styled';
 
 const UserDataPanel = () => {
   const { githubState } = useGithub();
+  const userData = [
+    {
+      title: 'Followers:',
+      content: githubState.user.followers,
+    },
+    {
+      title: 'Following:',
+      content: githubState.user.following,
+    },
+    {
+      title: 'Gists:',
+      content: githubState.user.public_gists,
+    },
+    {
+      title: 'Repositories:',
+      content: githubState.user.public_repos,
+    },
+    {
+      title: 'Location:',
+      content: !!githubState.user.location ? githubState.user.location : '-',
+    },
+    {
+      title: 'Company:',
+      content: !!githubState.user.company ? githubState.user.githubState : '-',
+    },
+  ];
   return (
     <div>
-      <S.Stats>
-        <S.StatItem>
-          <span>Followers:</span>
-          <p>{githubState.user.followers}</p>
-        </S.StatItem>
-        <S.StatItem>
-          <span>Following:</span>
-          <p>{githubState.user.following}</p>
-        </S.StatItem>
-        <S.StatItem>
-          <span>Gists:</span>
-          <p>{githubState.user.public_gists}</p>
-        </S.StatItem>
-        <S.StatItem>
-          <span>Repositories:</span>
-          <p>{githubState.user.public_repos}</p>
-        </S.StatItem>
-        <S.StatItem>
-          <span>Location:</span>
-          <p>{!!githubState.user.location ? githubState.user.location : '-'}</p>
-        </S.StatItem>
-        <S.StatItem>
-          <span>Company:</span>
-          <p>{!!githubState.user.company ? githubState.user.company : '-'}</p>
-        </S.StatItem>
-      </S.Stats>
+      <S.UserDataWrapper>
+        {userData.map((item, i) => (
+          <UserDataItem title={item.title} content={item.content} key={i} />
+        ))}
+      </S.UserDataWrapper>
     </div>
   );
 };
