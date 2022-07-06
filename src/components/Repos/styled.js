4d5578a6ef20f141d61bca3b25a52d597a14d2b8
Adriel <adriel.fariasan@gmail.com>
2022-07-06 00:27:06 -0300

    Reestiliza componente Repos

--- src/components/Repos/styled.js
+++ src/components/Repos/styled.js
@@ -2,33 +2,53 @@ import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import styled from 'styled-components';
 
 export const WrapperTabs = styled(Tabs)`
-  font-size: 12px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  flex-grow: 1;
+  margin-top: 15px;
 `;
 
 export const WrapperTabList = styled(TabList)`
-  list-style-type: none;
-  padding: 0 4px;
   display: flex;
+  list-style-type: none;
   gap: 10px;
   margin: 0;
+  padding: 0;
+  position: sticky;
+  top: 10px;
+  z-index: 10;
 `;
 
 WrapperTabList.tabsRole = 'TabList';
 
 export const WrapperTab = styled(Tab)`
   cursor: pointer;
-  border-radius: 4px 4px 0 0;
-  border: 1px solid #ccc;
-  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 2px ${({ theme }) => theme.accent};
+  padding: 8px;
   user-select: none;
+  font-size: 1.3rem;
+  transition: 0.5s all ease;
+  color: ${({ theme }) => theme.text};
+  background: ${({ theme }) => theme.bgAlt};
+  z-index: 10;
+
+  &:hover {
+    color: ${({ theme }) => theme.accent};
+    flex-grow: 1;
+  }
 
   &:focus {
     outline: none;
   }
 
   &.is-selected {
-    border-bottom: 1px solid #fff;
+    font-weight: 600;
+    background: ${({ theme }) => theme.accent};
+    color: ${({ theme }) => theme.accentContent};
+    flex-grow: 2;
   }
 `;
 
@@ -36,14 +56,13 @@ WrapperTab.tabsRole = 'Tab';
 
 export const WrapperTabPanel = styled(TabPanel)`
   display: none;
-  min-height: 40vh;
+  flex-grow: 1;
   padding: 8px;
-  border: 1px #ccc solid;
-  border-radius: 2px;
+  border-radius: 5px;
+  box-shadow: 0 0 2px ${({ theme }) => theme.accent};
 
   &.is-selected {
     display: block;
-    margin-top: -1px;
   }
 `;
 
