f8f715e4ce68ea9cb9abeb969912e0bc561d19d4
Adriel <adriel.fariasan@gmail.com>
2022-07-05 23:03:49 -0300

    Altera cores bg e bgAlt de tema escuro

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -7,8 +7,8 @@ export const lightTheme = {
 };
 
 export const darkTheme = {
-  bg: '#1f1f1f',
-  bgAlt: '#111',
+  bg: '#111',
+  bgAlt: '#1f1f1f',
   text: '#fff',
   accent: '#34c436',
   accentContent: '#fff',
