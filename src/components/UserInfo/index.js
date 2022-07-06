87e9c50855e2e4fe00d13f3aef0e8aa1f2860cb7
Adriel <adriel.fariasan@gmail.com>
2022-07-05 23:49:26 -0300

    Adiciona componente UserLinkItem

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,9 +1,34 @@
 import { Envelope, Github, Globe, Twitter } from 'react-bootstrap-icons';
 import useGithub from '../../hooks/github-hooks';
+import UserLinkItem from '../UserLinkItem';
 import * as S from './styled';
 
 const UserInfo = () => {
   const { githubState } = useGithub();
+  const links = [
+    {
+      icon: <Envelope alt='Send email' title='Send email' />,
+      linkUrl: !!githubState.user.email
+        ? `mailto:${githubState.user.email}`
+        : false,
+    },
+    {
+      icon: <Github alt='Visit github profile' title='Visit github profile' />,
+      linkUrl: githubState.user.html_url,
+    },
+    {
+      icon: <Globe alt='Visit website' title='Visit website' />,
+      linkUrl: githubState.user.blog,
+    },
+    {
+      icon: (
+        <Twitter alt='Visit twitter profile' title='Visit twitter profile' />
+      ),
+      linkUrl: !!githubState.user.twitter_username
+        ? `https://twitter.com/${githubState.user.twitter_username}`
+        : false,
+    },
+  ];
   return (
     <S.UserInfoWrapper>
       <S.UserPhotoWrapper>
@@ -14,40 +39,18 @@ const UserInfo = () => {
         </div>
       </S.UserPhotoWrapper>
       <S.UserSocialLinks>
-        <S.UserLink
-          href={githubState.user.html_url}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <Github />
-        </S.UserLink>
-        {githubState.user.blog ? (
-          <S.UserLink
-            href={githubState.user.blog}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <Globe />
-          </S.UserLink>
-        ) : null}
-        {githubState.user.twitter_username ? (
-          <S.UserLink
-            href={`https://twitter.com/${githubState.user.twitter_username}`}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <Twitter />
-          </S.UserLink>
-        ) : null}
-        {githubState.user.email ? (
-          <S.UserLink
-            href={`mailto:${githubState.user.email}`}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <Envelope />
-          </S.UserLink>
-        ) : null}
+        {links.map((linkItem, i) => {
+          if (!!linkItem.linkUrl) {
+            return (
+              <UserLinkItem
+                linkUrl={linkItem.linkUrl}
+                icon={linkItem.icon}
+                key={i}
+              />
+            );
+          }
+          return null;
+        })}
       </S.UserSocialLinks>
     </S.UserInfoWrapper>
   );
