28ae00c9ac57520b68f62c03cdd8dca70d4263d6
Adriel <adriel.fariasan@gmail.com>
2022-07-05 23:08:56 -0300

    Reestiliza componente Profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,44 +1,86 @@
 import useGithub from '../../hooks/github-hooks';
 import * as S from './styled';
+import { Github, Globe, Twitter, Envelope } from 'react-bootstrap-icons';
 
 const Profile = () => {
   const { githubState } = useGithub();
   return (
     <S.Wrapper>
-      <S.WrapperImage src={githubState.user.avatar_url} alt='User profile' />
-      <S.WrapperInfoUser>
-        <div>
-          <h1>{githubState.user.name}</h1>
-          <S.WrapperUsername>
-            <h3>{githubState.user.login}</h3>
-            <a
-              href={githubState.user.html_url}
+      <S.BasicInfo>
+        <S.UserInfo>
+          <img src={githubState.user.avatar_url} alt='User profile' />
+          <div>
+            {githubState.user.name ? <h2>{githubState.user.name}</h2> : null}
+            <p>{githubState.user.login}</p>
+          </div>
+        </S.UserInfo>
+        <S.UserLinks>
+          <S.UserLink
+            href={githubState.user.html_url}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Github />
+          </S.UserLink>
+          {githubState.user.blog ? (
+            <S.UserLink
+              href={githubState.user.blog}
               target='_blank'
               rel='noreferrer'
             >
-              {githubState.user.login}
-            </a>
-          </S.WrapperUsername>
-        </div>
-        <S.WrapperStatusCount>
-          <div>
-            <h4>Followers</h4>
-            <span>{githubState.user.followers}</span>
-          </div>
-          <div>
-            <h4>Following</h4>
-            <span>{githubState.user.following}</span>
-          </div>
-          <div>
-            <h4>Gists</h4>
-            <span>{githubState.user.public_gists}</span>
-          </div>
-          <div>
-            <h4>Repos</h4>
-            <span>{githubState.user.public_repos}</span>
-          </div>
-        </S.WrapperStatusCount>
-      </S.WrapperInfoUser>
+              <Globe />
+            </S.UserLink>
+          ) : null}
+          {githubState.user.twitter_username ? (
+            <S.UserLink
+              href={`https://twitter.com/${githubState.user.twitter_username}`}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Twitter />
+            </S.UserLink>
+          ) : null}
+          {githubState.user.email ? (
+            <S.UserLink
+              href={`mailto:${githubState.user.email}`}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Envelope />
+            </S.UserLink>
+          ) : null}
+        </S.UserLinks>
+      </S.BasicInfo>
+      <div>
+        <S.Stats>
+          <S.StatItem>
+            <span>Followers:</span>
+            <p>{githubState.user.followers}</p>
+          </S.StatItem>
+          <S.StatItem>
+            <span>Following:</span>
+            <p>{githubState.user.following}</p>
+          </S.StatItem>
+          <S.StatItem>
+            <span>Gists:</span>
+            <p>{githubState.user.public_gists}</p>
+          </S.StatItem>
+          <S.StatItem>
+            <span>Repositories:</span>
+            <p>{githubState.user.public_repos}</p>
+          </S.StatItem>
+          <S.StatItem>
+            <span>Location:</span>
+            <p>
+              {!!githubState.user.location ? githubState.user.location : '-'}
+            </p>
+          </S.StatItem>
+          <S.StatItem>
+            <span>Company:</span>
+            <p>{!!githubState.user.company ? githubState.user.company : '-'}</p>
+          </S.StatItem>
+        </S.Stats>
+      </div>
     </S.Wrapper>
   );
 };
--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,62 +1,136 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
-`;
+export const Wrapper = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
 
-export const WrapperStatusCount = styled.div`
-  display: flex;
-  align-items: center;
-  div {
-    margin: 8px;
-    text-align: center;
+  @media screen and (min-width: 768px) {
+    & {
+      grid-template-columns: 2fr 5fr;
+    }
   }
 `;
 
-export const WrapperInfoUser = styled.div`
+const commonStyle = styled.div`
+  width: 100%;
+  padding: 8px;
+  background: ${({ theme }) => theme.bgAlt};
+  border-radius: 5px;
+  box-shadow: 0 0 2px ${({ theme }) => theme.accent};
+`;
+
+export const BasicInfo = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  height: auto;
-  margin-left: 8px;
-  height: 200px;
+  gap: 10px;
 
-  h1,
-  h3,
-  h4 {
-    font-size: 32px;
-    font-weight: bold;
-  }
-
-  h3 {
-    font-size: 18px;
-  }
-
-  h4 {
-    font-size: 16px;
+  @media screen and (min-width: 768px) {
+    & {
+      position: sticky;
+      top: 10px;
+    }
   }
 `;
 
-export const WrapperUsername = styled.div`
+export const UserInfo = styled(commonStyle)`
   display: flex;
+  flex-direction: row;
   align-items: center;
-  margin-top: 8px;
+  gap: 10px;
 
-  h3 {
-    margin-right: 8px;
+  img {
+    width: 30%;
+    border-radius: 5px;
   }
 
-  a {
-    color: blue;
-    font-weight: bold;
-    font-size: 18px;
+  h2,
+  p {
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+    margin: 0;
+    padding: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    & {
+      flex-direction: column;
+      gap: 4px;
+    }
+
+    img {
+      width: 100%;
+    }
   }
 `;
 
-export const WrapperImage = styled.img`
-  border-radius: 50%;
-  width: 200px;
-  margin: 8px;
+export const Stats = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+
+  @media screen and (min-width: 768px) {
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+export const StatItem = styled(commonStyle)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  transition: 0.3s color ease, 0.3s box-shadow ease;
+
+  &:hover {
+    color: ${({ theme }) => theme.accent};
+    box-shadow: 0 0 3px ${({ theme }) => theme.accent};
+  }
+
+  p {
+    display: inline;
+    font-size: 1.15rem;
+    padding: 0;
+    margin: 0;
+    text-align: right;
+  }
+`;
+
+export const UserLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+`;
+
+export const UserLink = styled.a`
+  padding: 8px;
+  background: ${({ theme }) => theme.bgAlt};
+  border-radius: 5px;
+  box-shadow: 0 0 2px ${({ theme }) => theme.accent};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  color: ${({ theme }) => theme.text};
+  transition: 0.3s box-shadow ease, 0.3s color ease;
+
+  svg {
+    font-size: 28px;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.accent};
+    box-shadow: 0 0 3px ${({ theme }) => theme.accent};
+  }
 `;
